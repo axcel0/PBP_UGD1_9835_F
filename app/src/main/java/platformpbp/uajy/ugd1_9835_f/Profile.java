7d83b567d0b4d125fb8e0607e89a74473ba08dc4
axcel0 <axcelsantoso0@gmail.com>
2020-08-31 00:46:11 +0700

    UGD1_9835_F

--- app/src/main/java/platformpbp/uajy/ugd1_9835_f/Profile.java
+++ app/src/main/java/platformpbp/uajy/ugd1_9835_f/Profile.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Profile extends AppCompatActivity {
 
     private Button btnBack;
